6d4290d4ab8b4ad39b850f687dc86a53ff0d9650
nestort <nestrimo@gmail.com>
2019-10-25 17:49:30 -0500

    configuracion mongoose

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import { ApolloServer } from 'apollo-server';
+import mongoose  from 'mongoose';
+
 import typeDefs  from './graphql/schemas';
 import resolvers  from './graphql/resolvers';
 
+
+const url = process.env.DATA_BASE;
+mongoose.connect(
+  url,
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
+const mongoDB = mongoose.connection;
+mongoDB.on('error', console.error.bind(console, "¡Error de conexion!"));
+mongoDB.on('open', () => console.log('BD conectada'));
 
 
 
@@ -10,3 +26,4 @@ const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
+                        
