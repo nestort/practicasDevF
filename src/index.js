0f77a3855f3d4398fc7ec9b4ce74f87a3070c370
nestort <nestrimo@gmail.com>
2019-10-25 16:28:53 -0500

    Hola mundo con GraphQL

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-console.log("hola");
+const { ApolloServer, gql } = require('apollo-server');
+
+
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+const personData = [{
+  age: 19,
+  name: "Juan"
+}];
+
+
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Person {
+    name: String
+    age: Int
+  }
+
+  type Query {
+    books: [Book]
+    person: [Person]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+    person: () => personData,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
